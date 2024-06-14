2245c824d661a1eee039fb3b96a8a2faf1c3d1d2
Miguel Buitrago <miguelcatholic@gmail.com>
2024-06-14 07:41:30 -0500

    proyecto Miguel Buitrago

---
+++ proyectobase/proyecto_Miguel/script.js
@@ -0,0 +1,13 @@
+function rojo(){
+    let titulo=document.getElementById('titulo')
+    titulo.style.color="red"
+    
+}
+
+function azul(){
+    let titulo=document.getElementById('titulo')
+    titulo.style.color="blue"
+    
+}
+
+
