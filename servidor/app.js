67c7a36472c75cee1e5ccde712acf9cfbe5129a2
diegolegarda <legarda8027@gmail.com>
2024-06-17 08:07:02 -0500

    creacion de backend

---
+++ servidor/app.js
@@ -0,0 +1,9 @@
+const saludar=require('./modulosfuncionales/saludo')
+
+function sumar(a,b)
+{ let resultado;
+    resultado=a+b;
+    return resultado;
+}
+console.log(sumar(10,35));
+console.log(saludar('Juan'));
