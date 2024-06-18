3122b3c3bda16b461f6e29c062eb1112c0419658
vimapisas <pisavima@gmail.com>
2024-06-18 07:03:20 -0500

    años

--- servidor/app.js
+++ servidor/app.js
@@ -1,11 +1,13 @@
 const saludar=require('./modulosfuncionales/saludo')
+const sumar=require('./modulosfuncionales/operaciones');
+const operaciones = require('./modulosfuncionales/operaciones');
 
-function sumar(a,b)
-{let resultado;
-    resultado=a+b;
-    return resultado;
-}
-
-console.log(sumar(10,35));
-
+let inicio=100;
+let fin=200;
+let añosTotales=operaciones.sumar(inicio,fin)
+console.log("estos son los años totales"+añosTotales);
+console.log(operaciones.sumar(10,35));
+console.log(operaciones.restar(10,35));
+console.log(operaciones.multiplicar(10,35));
+console.log(operaciones.numeros);
 console.log (saludar("juan"))
--- servidor/modulosfuncionales/operaciones.js
+++ servidor/modulosfuncionales/operaciones.js
@@ -1,4 +1,24 @@
-const saludar=require ('.modulofuncionales/saludo')
-const sumar=require('./modulosfuncionales/operaciones')
+function sumar(a,b){
+    let resultado;
+    resultado=a+b;
+    return resultado;
+    
+}
+function restar(a,b){
+    let resultado;
+    resultado=a-b;
+    return resultado;
+}
 
-console.log(sumar(10,35))
+function multiplicar(a,b){
+
+    let resultado;
+    resultado=a*b;
+    return resultado;
+}
+const numeros=[1,5,7,9,3,12]];
+
+    module.exports={sumar:sumar,}
+    restar:restar,
+    multiplicar:multiplicar,
+    numeros:numeros]
