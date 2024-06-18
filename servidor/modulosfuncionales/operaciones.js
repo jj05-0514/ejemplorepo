dc8ab5607422eba192e0bc14819b6c442b1629f9
diegolegarda <legarda8027@gmail.com>
2024-06-18 08:05:55 -0500

    manejo de archivos con fs

--- servidor/modulosfuncionales/operaciones.js
+++ servidor/modulosfuncionales/operaciones.js
@@ -3,4 +3,19 @@ function sumar(a,b){
     resultado=a+b;
     return resultado;
 }
-module.exports=sumar;
+function restar(a,b){
+    let resultado;
+    resultado=a-b;
+    return resultado;
+}
+function multiplicar(a,b){
+    let resultado;
+    resultado=a*b;
+    return resultado;
+}
+const numeros=[1,5,7,9,3,12];
+
+module.exports={sumar:sumar,
+    restar:restar,
+    multiplicar:multiplicar,
+    numeros:numeros}
