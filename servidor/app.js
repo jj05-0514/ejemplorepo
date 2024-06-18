e8b8252af30c4e7d74527186413acfd6f1b6d07f
Germanken <germanken2020@outlook.com>
2024-06-18 08:07:44 -0500

    clase 18-06-24

--- servidor/app.js
+++ servidor/app.js
@@ -1,11 +1,49 @@
-const saludar=require('./modulosfuncionales/saludo')
+const saludar = require('./modulosfuncionales/saludo')
+const operaciones = require('./modulosfuncionales/operaciones')
+const { multiplicar } = require('./modulosfuncionales/operaciones')
+const fs = require('fs')
 
+let inicio = 100;
+let fin = 200;
+let añosTotales = operaciones.sumar(inicio, fin);
 
-    function sumar(a, b) {
-        let resultado;
-        resultado = a + b;
-        return resultado
-    }
-console.log(sumar(10, 35));
-
+console.log("Estos son los años totales " + añosTotales);
+let añosMultiplicados = multiplicar(inicio, fin);
+console.log("Estos son los años multiplicados " + añosMultiplicados);
+console.log(operaciones.sumar(10, 35));
+console.log(operaciones.restar(10, 35));
+console.log(operaciones.multiplicar(10, 35));
+console.log(operaciones.numeros);
 console.log(saludar('Juan'));
+console.warn("por favor incluir el nombre");
+console.error(new Error("el divisor no puede ser cero"));
+console.table(operaciones.numeros);
+
+
+setTimeout(operaciones.sumar, 2, 5, 3);
+console.log("antes de setInmediate");
+setImmediate(operaciones.sumar, 10, 3);
+console.log("despues de setInmediate");
+//setInterval(operaciones.sumar, 2, 1, 5);
+//console.log("antes de setInterval");
+
+//*********************************************** */
+fs.writeFile('./modulosfuncionales/archivo.txt', 'Hola mundo',
+    function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log('El archivo fue creado');
+        }
+    });
+fs.readFile('./modulosfuncionales/archivo.txt', function (err, contenido) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(contenido.toString())
+
+    }
+}
+);
