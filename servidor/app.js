11723485e7ff7e92d5de6d6870de6c53c54c27ed
JoseManuelPlazas <jmplazas2014@gmail.com>
2024-06-19 06:25:30 -0500

    nuevoArreglo

--- servidor/app.js
+++ servidor/app.js
@@ -1,9 +1,19 @@
 const saludar=require('./modulosfuncionales/saludo')
+const operaciones=require('./modulosfuncionales/operaciones')
+const {multiplicar}=require('/modulosfuncionales/operaciones')
 
-function sumar(a,b)
-{ let resultado;
-    resultado=a+b;
-    return resultado;
-}
-console.log(sumar(10,35));
+
+let inicio=100;
+let fin=200;
+let añosTotales=operaciones.sumar(inicio,fin);
+console.log("Estos son los años totales"+añosTotales);
+let añosMultiplicados=multiplicar(inicio,fin);
+console.log("Estos son los años multiplicados"+añosMultiplicados);
+console.log(operaciones.multiplicar(10,35));
+console.log(operaciones.numeros);
 console.log(saludar('Juan'));
+console.table(operaciones.numeros);
+
+// Manejo de archivos
+
+
---
+++ servidor/operaciones.js
@@ -0,0 +1,17 @@
+function sumar(a,b){
+     let resultado;
+     resultado=a+b;
+     return resultado;
+}
+function restar(a,b){
+    let resultado;
+    resultado=a-b;
+    return resultado;
+}
+function multiplicar(a,b){
+    let resultado=a*b;
+    return resultado;
+}
+const numeros=[1,5,7,9,3,12];
+
+module.exports={sumar:sumar,restar:restar,multiplicar:multiplicar,numeros:numeros}
