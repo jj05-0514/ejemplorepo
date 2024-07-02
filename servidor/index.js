9138b8d83d326c1f38160ff20f0a7161b709b313
Germanken <germanken2020@outlook.com>
2024-07-02 06:07:18 -0500

    cambio 4

--- servidor/index.js
+++ servidor/index.js
@@ -52,19 +52,33 @@ app.post('/formulario',validacionFormulario,(req,res)=>{
   const nombre=req.body.nombre;
   const correo=req.body.email;
   const contrasena=req.body.password;
+  cont=cont+1;
   console.log(`Hola ${nombre}! tu correo es ${correo} y tu contraseña es ${contrasena}`)
+  const persona1={id:cont, nombre:nombre, correo:correo, contrasena:contrasena};
+  personas.push(persona1);
   res.send(`Hola ${nombre}! tu correo es ${correo} y tu contraseña es ${contrasena}`);  
 })
 
+//************************************* */
+//  CRUD para un arreglo
+const personas=[{id:1, nombre:"juan", correo:"juan@mail.com",contrasena:"123456"}];
+let cont=1;
 
+app.get('/api/personas', (req,res)=>{
+  console.log(personas);
+  res.json(personas);
+});
 
-app.listen(port, () => {
-  console.log(`Servidor activo escuchando en el puerto http://localhost:${port}`)
-=======
-app.get('/', (req, res)=> {
-    res.send('hola mundo!')
+app.get('/api/personas/usuario',(req,res)=>{
+  const id=req.body.id;
+  console.log("este es el indentificador", id);
+  const persona=personas.find ((persona)=>persona.id==id);
+if(!persona){
+  res.status(404).send('No se encontro a la persona')
 })
-
+  console.log(personas[id]);
+  res.json(personas[id]);
+});
 app.listen(port, () => {
     console.log(`servidor activo escuchando en el puerto http://localhost:${port}`)
 
