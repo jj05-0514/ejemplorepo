db67facfa7b94af197e18d5bc2d20428ca4a868a
Miguel Buitrago <miguelcatholic@gmail.com>
2024-07-10 08:08:21 -0500

    subiendo a Miguelón

--- servidor/paginas/programa.js
+++ servidor/paginas/programa.js
@@ -86,3 +86,84 @@ async function buscarUsuario(){
     }
 }
   document.getElementById('boton2').addEventListener('click',buscarUsuario);
+
+  async function buscarUsuario2(){
+    const nomb = document.getElementById('busqueda')
+    console.log(nomb);
+    try{
+        const respuesta= await axios.post('/usuario/nombre',{
+            nombre:nomb })
+        document.getElementById('respuesta2').innerHTML=`${respuesta.data.nombre} ${respuesta.data.correo} ${respuesta.data.contrasena}` ;
+        console.log(respuesta.data);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+document.getElementById('boton3').addEventListener('click',buscarUsuario);
+
+
+
+/********* */
+// actualizar un usuario por nombre
+
+async function cargarDatos2() {
+    try{
+        const respuesta= await axios.get('/usuarios')        
+    if (respuesta.data) {           
+      const userList = document.getElementById('listaUsuarios2');
+      userList.innerHTML = '';
+respuesta.data.forEach(user => {
+        const option = document.createElement('option');
+        option.value = `${user.nombre}`;
+        option.textContent = `${user.nombre}`;
+        userList.appendChild(option);
+      });
+    }
+}
+catch(error){
+    console.log(error); 
+  }      
+}
+cargarDatos2();
+
+async function llenarDatos(){
+    const nomb = document.getElementById('listaUsuarios2').value;
+    console.log(nomb);
+    try{
+        const respuesta= await axios.post('/usuario/nombre',{
+            nombre:nomb })
+document.getElementById('nuevoNombre').value=`${respuesta.data.nombre}`;
+        document.getElementById('nuevoCorreo').value=`${respuesta.data.correo}`;
+        document.getElementById('nuevaContrasena').value=`${respuesta.data.contrasena}`;
+        console.log(respuesta.data);
+    }
+catch(error){
+        console.log(error);
+    }
+}
+async function actualizarRegistro(){
+    const nomb = document.getElementById('listaUsuarios2').value;
+    const nombnuevo=document.getElementById('nuevoNombre').value;
+    const corr=document.getElementById('nuevoCorreo').value;
+    const contra=document.getElementById('nuevaContrasena').value;
+try{
+        const respuesta= await axios.post('/usuario/actualizacionNombre',{
+           nombre:nomb,
+           nuevonombre:nombnuevo,
+           nuevocorreo:corr,
+           nuevopassword:contra
+        });
+        if(respuesta.data){
+            document.getElementById('respuesta3').innerHTML=respuesta.data;
+            cargarDatos();
+            cargarDatos2();
+        }
+        document.getElementById('respuesta3').innerHTML=respuesta.data;
+    } catch(error){
+        console.log(error);
+    }
+
+}
+document.getElementById('listaUsuarios2').addEventListener('change',llenarDatos);
+document.getElementById('boton4').addEventListener('click',actualizarRegistro);
