8673a47404e5568fdfb55c0475540c4d0a8445d4
diegolegarda <legarda8027@gmail.com>
2024-07-24 00:08:14 -0500

    estructuracion de rutas y middleware

--- servidor/index.js
+++ servidor/index.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const path = require('path');
 const app = express()
 const port = 3000
+const rutasPaginas = require('./rutas/rutasPaginas');
+const connectDB = require('./conexiones/conexionBD');
+const BaseDatos = require('./rutas/rutasBD');
 
 
 //************************************************************ */
@@ -13,56 +16,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+/*********************************************** */
+// enrutamiento para servir paginas
+app.use('/', rutasPaginas);
 
-//******************************************* */
-//        middleware de rutas
-app.use((req, res, next) => {
-  console.log('Middleware de rutas');
-  next();
-})
-
-
+/********************************************* */
+// enrutamiento para operaciones con la base de datos
+app.use('/bd', BaseDatos);
 
 //************************************************************ */
-//  rutas de api
-app.get('/', (req, res) => {
-  res.send('Hola mundo!')
-})
+//        middleware para manejo de errores
+app.use((req, res) => {
+  res.status(404).send('Pagina no encontrada');
+});
 
-app.get('/saludo/:nombre', (req, res) => {
-  const nombre = req.params.nombre;
-  res.send(`Hola ${nombre}!`);
-})
-
-app.post('/formulario', validacionFormulario, (req, res) => {
-  const nombre = req.body.nombre;
-  const correo = req.body.email;
-  const contrasena = req.body.password;
-  console.log(`Hola ${nombre}! tu correo es ${correo} y tu contraseña es ${contrasena}`)
-  res.send(`Hola ${nombre}! tu correo es ${correo} y tu contraseña es ${contrasena}`);
-})
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo salió mal!');
+});
 
 
-
-
-
+/*********************************************** */
+// conexion a base datos
 connectDB();
-
-
-
-
-
-
-
-
-
-
-//*************************************** */
-//      Ruta para usuario creado
-
-
 
 app.listen(port, () => {
   console.log(`Servidor activo escuchando en el puerto http://localhost:${port}`)
 })
-
--- servidor/rutas/rutasPaginas.js
+++ servidor/rutas/rutasPaginas.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const vistas=require('./controladores/controladoresPaginas.js')
+const vistas=require('../controladores/controladorPaginas.js')
+
 
 router.get('/', (req, res) => {
     res.send('Hola mundo!')
@@ -11,3 +12,5 @@ router.get('/servicios',vistas.pagServicios);
 router.get('/nosotros',vistas.pagNosotros);
 router.get('/error',vistas.pagError);
 router.get('/usuarioCreado',vistas.pagUsuarioCreado);
+
+module.exports = router;
